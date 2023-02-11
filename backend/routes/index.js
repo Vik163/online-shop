987cb8d1931e3e6253f3c14fb003dfc143e4a4ea
Vik63 <sfoto116@yandex.ru>
2023-02-11 18:41:04 +0400

    1-9

--- backend/routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.use('/cards', require('./stores'));
-
-module.exports = router;
--- backend/routes/stores.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const { addCard, updateCard, getCards } = require('../controllers/stores');
-
-router.get('/', getCards);
-router.post('/', addCard);
-router.patch('/', updateCard);
-
-module.exports = router;
